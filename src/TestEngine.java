770bf9b05fab50b4287da3c39e4f020ec093d89f
msha1011 <muhtady.sharker@city.ac.uk>
2026-02-16 09:54:23 +0000

    Inital game project

---
+++ src/TestEngine.java
@@ -0,0 +1,8 @@
+import city.cs.engine.*;
+
+public class TestEngine {
+    public static void main(String[] args) {
+        World world = new World();
+        System.out.println("City Engine working: " + world);
+    }
+}
