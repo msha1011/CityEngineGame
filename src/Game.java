23adf2b1de277fbc749fca3993ae3aceed91fbfb
msha1011 <muhtady.sharker@city.ac.uk>
2026-02-21 19:46:31 +0000

    Inital game project Created  Car Class with inheritance and encapsualtion Created Coin Class using inheritance and encapusaltion Created GameWorld class and added Car object Created GameView class extending UserView Added Game class to initialise world, view, and start simulation using encapsulation and inheritance

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,36 @@
+import city.cs.engine.*;
+import javax.swing.JFrame;
+
+// Main entry point of the game
 public class Game {
+
+    // Encapsulation: private fields
+    private GameWorld world;
+    private GameView view;
+
+    public Game() {
+
+        // Create world
+        world = new GameWorld();
+
+        // Create view
+        view = new GameView(world, 800, 600);
+
+        // Create window
+        JFrame frame = new JFrame("Car Coin Game");
+
+        frame.add(view);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+
+        // Start physics
+        world.start();
+    }
+
+    // Main method (PROGRAM STARTS HERE)
+    public static void main(String[] args) {
+        new Game();
+    }
 }
