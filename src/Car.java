f202b51e2b58c580cb44c1815641a85d699fa9ca
msha1011 <muhtady.sharker@city.ac.uk>
2026-02-22 16:32:13 +0000

    Inital game project Created  Car Class with inheritance and encapsualtion Created Coin Class using inheritance and encapusaltion Created GameWorld class and added Car object Created GameView class extending UserView Added Game class to initialise world, view, and start simulation using encapsulation and inheritance added movements Milestone 1 complete: player movement, coin spawning, collision detection, scoring system implemented destroy coin when hitting the floor add score on screen and change colour milestone one compelte

--- src/Car.java
+++ src/Car.java
@@ -1,23 +1,16 @@
 import city.cs.engine.*;
-import org.jbox2d.common.Vec2;
 
-// Player car body
 public class Car extends DynamicBody {
 
-    // Shared shape and image for all cars
+    private int score;
+
     private static final Shape carShape = new BoxShape(1.5f, 0.75f);
     private static final BodyImage carImage = new BodyImage("data/car.png", 2f);
-
-    // Encapsulation: score is private
-    private int score;
 
     public Car(World world) {
         super(world, carShape);
         addImage(carImage);
-
         score = 0;
-
-
     }
 
     public int getScore() {
@@ -26,19 +19,5 @@ public class Car extends DynamicBody {
 
     public void addScore(int amount) {
         score += amount;
-        System.out.println("Score: " + score);
-    }
-
-    // Move left/right (called later)
-    public void moveLeft() {
-        setLinearVelocity(new Vec2(-8f, getLinearVelocity().y));
-    }
-
-    public void moveRight() {
-        setLinearVelocity(new Vec2(8f, getLinearVelocity().y));
-    }
-
-    public void stopMoving() {
-        setLinearVelocity(new Vec2(0f, getLinearVelocity().y));
     }
 }
