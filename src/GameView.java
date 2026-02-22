b2083099ea4b5427a1da78d548b710726ddf7674
msha1011 <muhtady.sharker@city.ac.uk>
2026-02-22 00:26:52 +0000

    Inital game project Created  Car Class with inheritance and encapsualtion Created Coin Class using inheritance and encapusaltion Created GameWorld class and added Car object Created GameView class extending UserView Added Game class to initialise world, view, and start simulation using encapsulation and inheritance added movements Milestone 1 complete: player movement, coin spawning, collision detection, scoring system implemented destroy coin when hitting the floor add score on screen and change colour

--- src/GameView.java
+++ src/GameView.java
@@ -1,9 +1,24 @@
-import city.cs.engine.UserView;
+import city.cs.engine.*;
+import javax.swing.*;
+import java.awt.*;
 
-// This is the camera/view on the world
 public class GameView extends UserView {
+
+    private GameWorld world;
 
     public GameView(GameWorld world, int width, int height) {
         super(world, width, height);
+        this.world = world;
+    }
+
+    @Override
+    protected void paintForeground(Graphics2D g) {
+
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 24));
+
+        int score = world.getCar().getScore();
+
+        g.drawString("Score: " + score, 20, 30);
     }
 }
