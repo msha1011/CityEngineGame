f42df7c3fdc544be6f9a72f5c59c5712f2970197
msha1011 <muhtady.sharker@city.ac.uk>
2026-02-22 00:13:11 +0000

    Inital game project Created  Car Class with inheritance and encapsualtion Created Coin Class using inheritance and encapusaltion Created GameWorld class and added Car object Created GameView class extending UserView Added Game class to initialise world, view, and start simulation using encapsulation and inheritance added movements Milestone 1 complete: player movement, coin spawning, collision detection, scoring system implemented

--- src/TestEngine.java
+++
@@ -1,8 +0,0 @@
-import city.cs.engine.*;
-
-public class TestEngine {
-    public static void main(String[] args) {
-        World world = new World();
-        System.out.println("City Engine working: " + world);
-    }
-}
